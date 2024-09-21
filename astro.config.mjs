21b2b284905a6b482c60e48898d950db3f9e0ac4
ignacio castillo <castilloignacio1510@gmail.com>
2024-09-21 13:53:38 -0600

    ahora tiene el adaptador de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'server'
-})
+  output: 'server',  // O 'hybrid' si lo prefieres
+  adapter: vercel(),
+});
